8d32d09aa8c1537f2026da6ece673de6d144b58f
Aminul Islam <aminul-xs@users.noreply.github.com>
2024-10-13 08:49:33 +0600

    Refactor component files and update build configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+// 'use client'
+
 import Border from "./component/Border";
 import Color from "./component/Color";
 
-export { Border, Color };
+export { Border, Color, };
+
+
+// export { Link } from './link'
+// export { ViewTransitions } from './transition-context'
+// export { useTransitionRouter } from './use-transition-router'
